a736d9ecbc6d41762cce1cab3e373ac4ca3e35ba
Regawa <regawa04@gmail.com>
2019-05-10 20:48:21 +0700

    Add Login and Logout Feature for Admin

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <html>
 <head>
-<title>Hello</title>
+	<title>Sistem Informasi Inventaris Aset</title>
 </head>
+<body>
+	<h1>HOME</h1>
+	<a href="login-form.php">Login</a>
+	<a href="login-form.php">Admin</a>
+</form>
+</body>
 </html>
