3580f7e362cf63a4f74bb87590bcf8d21a685db8
sulemigoreng <sulemigoreng@gmail.com>
2019-05-11 11:05:48 +0700

    Adding Landing Page, Login Page, Dashboard
    Adding Landing Page, Login Page, Dashboard (UI Only)

--- index.php
+++ index.php
@@ -1,11 +1,3 @@
-<html>
-<head>
-	<title>Sistem Informasi Inventaris Aset</title>
-</head>
-<body>
-	<h1>HOME</h1>
-	<a href="login-form.php">Login</a>
-	<a href="login-form.php">Admin</a>
-</form>
-</body>
-</html>
+<?php
+    header("location:link/beranda.php");
+?>
---
+++ link/login.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+	<link rel="stylesheet" type="text/css" href="../css/animate.css">
+	<link rel="stylesheet" type="text/css" href="../css/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="../css/util.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+</head>
+<body>
+    
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<form class="login100-form validate-form">
+					<span class="login100-form-title p-b-43">
+						Login to continue
+					</span>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="email">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Email</span>
+					</div>
+					
+					
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<input class="input100" type="password" name="pass">
+						<span class="focus-input100"></span>
+						<span class="label-input100">Password</span>
+					</div>
+
+					<div class="flex-sb-m w-full p-t-3 p-b-32">
+
+						<div>
+							<a href="#" class="txt1">
+								Forgot Password?
+							</a>
+						</div>
+					</div>
+			
+
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+					
+					<div class="text-center p-t-46 p-b-20">
+						<span class="txt2">
+							<a href="beranda.php">< Kembali ke Beranda</a>
+                        </span>
+                        <span class="txt2">
+							<a href="admin/dashboard.php">Ke Dashboard ></a>
+                        </span>
+                        
+					</div>
+				</form>
+
+				<div class="login100-more" style="background-image: url('../img/bg-01.jpg');">
+				</div>
+			</div>
+		</div>
+    </div>
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+    <script src="../js/login.js"></script>
+    <script src="../js/popper.js"></script>
+    <script src="../js/animsition.js"></script>
+    <script src="../js/select2.js"></script>
+	<script src="../js/moment.js"></script>
+	<script src="../js/daterangepicker.js"></script>
+
+</body>
+</html>
