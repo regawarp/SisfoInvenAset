05fec322bc6cb4c8135d2345392ea4be8e746a81
sulemigoreng <sulemigoreng@gmail.com>
2019-05-14 15:22:12 +0700

    Update Revisi Beranda

--- link/beranda.php
+++ link/beranda.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>PUPR</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>PUPR</title>
 
-    <!-- Google font -->
+	<!-- Google font -->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7CVarela+Round" rel="stylesheet">
-    <!-- Bootstrap -->
-    <link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" />
+	<!-- Bootstrap -->
+	<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" />
 
-    <!-- Owl Carousel -->
-    <link type="text/css" rel="stylesheet" href="../css/owl.carousel.css" />
-    <link type="text/css" rel="stylesheet" href="../css/owl.theme.default.css" />
+	<!-- Owl Carousel -->
+	<link type="text/css" rel="stylesheet" href="../css/owl.carousel.css" />
+	<link type="text/css" rel="stylesheet" href="../css/owl.theme.default.css" />
 
-    <!-- Magnific Popup -->
-    <link type="text/css" rel="stylesheet" href="../css/magnific-popup.css" />
+	<!-- Magnific Popup -->
+	<link type="text/css" rel="stylesheet" href="../css/magnific-popup.css" />
 
-    <!-- Font Awesome Icon -->
-    <link rel="stylesheet" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!-- Font Awesome Icon -->
+	<link rel="stylesheet" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 
-    <!-- Custom stlylesheet -->
-    <link type="text/css" rel="stylesheet" href="../css/style.css" />
+	<!-- Custom stlylesheet -->
+	<link type="text/css" rel="stylesheet" href="../css/style.css" />
 
 </head>
+
 <body>
-    <!-- Header -->
+	<!-- Header -->
 	<header id="home">
 		<!-- Background Image -->
 		<div class="bg-img" style="background-image: url('../img/background1.jpg');">
-			<div class="overlay"></div>
+			<!-- <div class="overlay"></div> -->
 		</div>
 		<!-- /Background Image -->
 
@@ -59,12 +61,14 @@
 				<ul class="main-nav nav navbar-nav navbar-right">
 					<li><a href="#home">Beranda</a></li>
 					<li><a href="#about">Tentang</a></li>
+					<li><a href="#peta">Peta</a></li>
 					<li class="has-dropdown"><a href="#fitur">Fitur</a>
 						<ul class="dropdown">
 							<li class="has-dropdown"><a href="#">Irigasi</a>
 								<ul class="dropdown">
 									<li><a href="#">Dana Alokasi Khusus</a></li>
 									<li><a href="#">Irigasi teknis</a></li>
+									<li><a href="#">Tanah</a></li>
 									<li><a href="#">Biaya Operasi dan Maintenance</a></li>
 									<li><a href="#">Konstruksi dalam Pengerjaan</a></li>
 									<li><a href="#">Peta Irigasi Teknis</a></li>
@@ -77,18 +81,41 @@
 									<li><a href="#">Drainase</a></li>
 									<li><a href="#">Danau</a></li>
 									<li><a href="#">Biaya Operasi dan Pemeliharaan</a></li>
+									<li><a href="#">Peta Titik Lokasi Banjir</a></li>
 								</ul>
 							</li>
 						</ul>
 					</li>
 					<li><a href="#contact">Kontak</a></li>
-                    <li><a href="login.php">Login</a></li>
+					<li><a href="#" data-toggle="modal" data-target="#exampleModalCenter">Login</a></li>
 				</ul>
 				<!-- /Main navigation -->
 
 			</div>
 		</nav>
 		<!-- /Nav -->
+
+		<!-- Modal Login -->
+		<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			<div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-label="Close" style="position:absolute;right:0;margin-right:20px;">
+							<span aria-hidden="true">&times;</span>
+						</button>
+						<h5 class="modal-title" id="exampleModalLongTitle">Login</h5>
+					</div>
+					<div class="modal-body">
+						<form action="" method="post">
+							<input type="text" name="username" id="" class="form-control">
+							<input type="password" name="password" id="" class="form-control">
+							<input type="submit" value="Login">
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- /Modal Login -->
 
 		<!-- home wrapper -->
 		<div class="home-wrapper">
@@ -98,7 +125,7 @@
 					<!-- home content -->
 					<div class="col-md-10 col-md-offset-1">
 						<div class="home-content">
-							<h1 class="white-text">Aplikasi Inventarisasi <br/>Aset Infrastruktur <br/>Sumber Daya Air</h1>
+							<h1 class="white-text bgjudul">Aplikasi Inventarisasi <br />Aset Infrastruktur <br />Sumber Daya Air</h1>
 							<!--<p class="white-text">Morbi mattis felis at nunc. Duis viverra diam non justo. In nisl. Nullam sit amet magna in magna gravida vehicula. Mauris tincidunt sem sed arcu. Nunc posuere.</p>-->
 							<!--<button class="white-btn">Get Started!</button>
 							<button class="main-btn">Learn more</button>-->
@@ -112,9 +139,10 @@
 		<!-- /home wrapper -->
 
 	</header>
-    <!-- /Header -->
-    <!-- Penjelasan Aplikasi -->
-	<div id="about" class="section md-padding bg-grey">
+	<!-- /Header -->
+
+	<!-- Penjelasan Aplikasi -->
+	<div id="about" class="section md-padding bg-white">
 
 		<!-- Container -->
 		<div class="container">
@@ -130,13 +158,13 @@
 					<p>Aplikasi ini adalah aplikasi yang akan membantu pengelola aset infrastruktur untuk melakukan serangkaian kegiatan pendataan,
 						pencatatan, serta memvisualisasikan aset infrastruktur sumber daya air agar tertib administrasi, pengamanan aset, serta pengendalian dan pengawasan aset.
 					</p>
-					
+
 				</div>
 				<!-- /Penjelasan Aplikasi content -->
 
 				<!-- About slider -->
 				<div class="col-md-3">
-						<img class="img-responsive" src="../img/logo-square.png" alt="logo-square-alt.png">
+					<img class="img-responsive" src="../img/logo-square.png" alt="logo-square-alt.png">
 				</div>
 				<!-- /About slider -->
 
@@ -146,10 +174,61 @@
 		</div>
 		<!-- /Container -->
 
-		</div>
-		<!-- /Penjelasan Aplikasi -->
+	</div>
+	<!-- /Penjelasan Aplikasi -->
 
-    <!-- About -->
+	<!-- Peta -->
+	<div id="peta" class="section md-padding bg-grey">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Peta content -->
+				<div class="section-header text-center">
+					<h2 class="title">Peta Aset Infrastruktur Sumber Daya Air</h2>
+				</div>
+				<div class="col-md-12 tentang">
+					<iframe width="950" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Aset Infrastruktur Sumber Daya Air" src="//www.arcgis.com/apps/Embed/index.html?webmap=ef284696a7a8437e8aac540dd4e680e3&extent=107.54,-7.4457,107.5849,-7.4224&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=details&basemap_gallery=true&disable_scroll=true&theme=light"></iframe>
+				</div>
+				<!-- /Peta content -->
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+
+	<div id="peta" class="section md-padding bg-grey">
+
+		<!-- Container -->
+		<div class="container">
+
+			<!-- Row -->
+			<div class="row">
+
+				<!-- Peta content -->
+				<div class="section-header text-center">
+					<h2 class="title">Peta Titik Lokasi Banjir</h2>
+				</div>
+				<div class="col-md-12 tentang">
+					<iframe width="950" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Peta Titik Lokasi Banjir" src="//www.arcgis.com/apps/Embed/index.html?webmap=cfc7c9ea3f984a29a9475890be44ad8b&extent=107.8132,-7.2523,107.9928,-7.1592&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=details&basemap_gallery=true&disable_scroll=true&theme=light"></iframe>
+				</div>
+				<!-- /Peta content -->
+			</div>
+			<!-- /Row -->
+
+		</div>
+		<!-- /Container -->
+
+	</div>
+
+	<!-- /Peta -->
+
+	<!-- About -->
 	<div id="fitur" class="section md-padding">
 
 		<!-- Container -->
