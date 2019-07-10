ef41f10fe05d3bb0b1233169ad12917de17ef187
sulemigoreng <sulemigoreng@gmail.com>
2019-07-11 03:50:12 +0700

    Update kibf_home nilai kontrak type to number

--- link/admin/kibf_home.php
+++ link/admin/kibf_home.php
@@ -329,7 +329,7 @@ if (isset($_SESSION['user_id'])) {
 													<div class="form-group row">
 														<label class="col-sm-3 col-form-label">Nilai Kontrak</label>
 														<div class="col-sm-9">
-															<input type="text" class="form-control" name="NILAI_KONTRAK" />
+															<input type="number" class="form-control" name="NILAI_KONTRAK" />
 														</div>
 													</div>
 												</div>
